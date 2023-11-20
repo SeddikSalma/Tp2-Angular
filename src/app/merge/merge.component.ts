e46676177047a924825ca589d07e920a7c5a6c4a
M0ngi <saidanemongi@gmail.com>
2023-11-20 13:56:24 +0100

    finish reduce

--- src/app/merge/merge.component.ts
+++ src/app/merge/merge.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, Subject, combineLatest, distinctUntilChanged, filter, map, merge, scan, tap, withLatestFrom } from 'rxjs';
+import { Observable, Subject, combineLatest, distinctUntilChanged, filter, map, merge, reduce, scan, tap, withLatestFrom } from 'rxjs';
 
 @Component({
   selector: 'app-merge',
@@ -10,31 +10,38 @@ import { Observable, Subject, combineLatest, distinctUntilChanged, filter, map, 
 export class MergeComponent {
   input1 = new FormControl('');
   input2 = new FormControl('');
+
   focusSubject = new Subject<number>()
   focus$ = this.focusSubject.asObservable()
 
-  val1$: Observable<number>
-  val2$: Observable<number>
+  val1Subject = new Subject<number>()
+  val1$: Observable<number> = this.val1Subject.asObservable()
+
+  val2Subject = new Subject<number>()
+  val2$: Observable<number> = this.val2Subject.asObservable()
 
   merge$: Observable<number>
   scan$: Observable<number>
+  reduce$: Observable<number>
 
   constructor() {
-    this.val1$ = this.focus$.pipe(
+    this.focus$.pipe(
       withLatestFrom(this.input1.valueChanges),
       map(([_, vals]) => {
         return parseInt(vals ?? "0")
       }),
-      distinctUntilChanged()
-    )
+      distinctUntilChanged(),
+      tap((value) => this.val1Subject.next(value))
+    ).subscribe()
 
-    this.val2$ = this.focus$.pipe(
+    this.focus$.pipe(
       withLatestFrom(this.input2.valueChanges),
       map(([_, vals]) => {
         return parseInt(vals ?? "0")
       }),
-      distinctUntilChanged()
-    )
+      distinctUntilChanged(),
+      tap((value) => this.val2Subject.next(value))
+    ).subscribe()
 
     this.merge$ = merge(
       this.val1$,
@@ -44,10 +51,21 @@ export class MergeComponent {
     this.scan$ = this.merge$.pipe(
       scan((prev, curr) => curr + prev, 0)
     )
+
+    this.reduce$ = this.merge$.pipe(
+      reduce((prev, curr) => curr + prev, 0)
+    )
   }
 
-  focusOut(event: FocusEvent) {
-    console.log(1)
+  finishStream1() {
+    this.val1Subject.complete()
+  }
+
+  finishStream2() {
+    this.val2Subject.complete()
+  }
+
+  focusOut() {
     this.focusSubject.next(1)
   }
 }
