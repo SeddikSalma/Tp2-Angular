18a79056af28ee4a45675212a7c78febe76608a8
M0ngi <saidanemongi@gmail.com>
2023-12-04 00:46:11 +0100

    condition

--- src/app/add-cv/add-cv.component.ts
+++ src/app/add-cv/add-cv.component.ts
@@ -1,7 +1,7 @@
 import { Personne } from './../cv/model/personne';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm, FormControl } from '@angular/forms';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { CvService } from '../cv/services/cv.service';
 import { CanComponentDeactivate } from './can-deactivate.guard';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
@@ -17,20 +17,30 @@ import { Observable, map } from 'rxjs';
 export class AddCvComponent implements CanComponentDeactivate {
 
   errorMessage = '';
+  @ViewChild('formulaire', { static: true }) myForm?: NgForm;
+
   constructor(
     private dialog: MatDialog,
     private cvService: CvService,
     private router: Router
-  ) { }
+  ) {
+
+  }
 
 
-  createcv(item : NgForm){
+  createcv(item: NgForm) {
     this.cvService.addPersonne(item.value);
     this.router.navigate(['cv']);
   }
 
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
-    if (0==0) {
+    console.log(this.myForm)
+    console.log(this.myForm?.pending)
+    console.log(this.myForm?.submitted)
+    console.log(this.myForm?.pending)
+    setTimeout(() => { console.log(this.myForm?.submitted); console.log(this.myForm?.pending) }, 2000)
+    // TODO: Not done yet
+    if (!this.myForm?.pending && this.myForm?.dirty) {
       const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
         width: '300px',
       });
